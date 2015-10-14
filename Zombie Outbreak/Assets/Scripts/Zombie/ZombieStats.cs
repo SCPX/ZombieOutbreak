e7be992390178a9d4d0c43b4ebbe37e1f41c98c4
Joshua Clark <jdc2008@gmail.com>
2015-10-13 21:54:53 -0400

    Added zombie stats
    + added a folder for zombie scripts
    + added a script for the stats
    * moved basic AI script to zombie folder

---
+++ Zombie Outbreak/Assets/Scripts/Zombie/ZombieStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieStats : MonoBehaviour {
+    public int health = 0;
+    public int damage = 0;
+    public int resistance = 0;
+    public int speed = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
