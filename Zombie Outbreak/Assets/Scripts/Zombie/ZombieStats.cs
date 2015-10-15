d9be0c430b79d6d20968304963bbdea8422aecee
Joshua Clark <jdc2008@gmail.com>
2015-10-15 18:39:31 -0400

    * Updated Zombie stats
    + a rigid body 2D to the zombie (currently not sure if I will use it)

--- Zombie Outbreak/Assets/Scripts/Zombie/ZombieStats.cs
+++ Zombie Outbreak/Assets/Scripts/Zombie/ZombieStats.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class ZombieStats : MonoBehaviour {
-    public int health = 0;
-    public int damage = 0;
+    public int health = 1;
+    public int damage = 1;
     public int resistance = 0;
-    public int speed = 0;
+    public int speed = 2;
 	// Use this for initialization
 	void Start () {
 	
