45bd506520bfb93dbbc07a2dddcf59b28aff4f3f
Joshua Clark <jdc2008@gmail.com>
2015-10-15 20:12:42 -0400

    + Added collisions with the bullet and player
    * Added collisions in basic ai to start dealing with environment

--- Zombie Outbreak/Assets/Scripts/Zombie/BasicAI.cs
+++ Zombie Outbreak/Assets/Scripts/Zombie/BasicAI.cs
@@ -10,6 +10,21 @@ public class BasicAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+    void Move()
+    {
+        //check for player
+        //if no player get random direction 180 degrees
+        //move speed in that direction
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Environment")
+        {
+            Debug.Log("Bumped into something");
+        }
+
+    }
 }
---
+++ Zombie Outbreak/Assets/Scripts/Zombie/ZombieCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("killed the player");
+        }else if(col.gameObject.tag == "PlayerBullet")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
