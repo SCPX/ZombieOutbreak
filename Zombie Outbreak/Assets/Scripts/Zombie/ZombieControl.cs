e14f7e5508b28b27290f5aebf3751d93b733a02b
Joshua Clark <jdc2008@gmail.com>
2015-10-21 16:35:12 -0400

    *Zombie sprite now faces its movement direction
    *commented my code

--- Zombie Outbreak/Assets/Scripts/Zombie/ZombieControl.cs
+++ Zombie Outbreak/Assets/Scripts/Zombie/ZombieControl.cs
@@ -7,14 +7,16 @@ public class ZombieControl : MonoBehaviour
     public float damage = 1;
     public float resistance = 0;
     public float speed = 2;
-    private Vector3 moveDirection = Vector3.zero;
-    bool stun = false;
-    private float timer = 0;
-    private const float MAXTIMER = 2;
-    private const float THRESHOLD = 15;
+    private Vector3 moveDirection = Vector3.zero; 
+    bool stun = false; //used in knockback
+    private float timer = 0; // used in AI to wander around
+    private float rot_z = 0; //used to rotate sprite
+    private const float MAXTIMER = 2; //used in AI to wander around
+    private const float THRESHOLD = 15;// used in AI to hunt player
 
     public float Health
     {
+        //acts as a variable with error checks built in
         get { return health; }
         set
         {
@@ -31,6 +33,7 @@ public class ZombieControl : MonoBehaviour
 
     void Start()
     {
+        //initiates rigid body and sets stun to false
         _rigidbody = GetComponent<Rigidbody2D>() as Rigidbody2D;
         if (_rigidbody == null)
         {
@@ -44,19 +47,22 @@ public class ZombieControl : MonoBehaviour
         if (!stun && timer <= 0f)
         {
             moveDirection = Vector3.zero;
-            //insert ai here
+            //this is the AI  it wanders until the player is found
             if (PlayerControl.Instance != null)
             {
                 if (Vector3.Distance(PlayerControl.Instance.transform.position, this.transform.position) < THRESHOLD)
                 {
                     moveDirection = (PlayerControl.Instance.transform.position - this.transform.position).normalized;
+                    rot_z = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;//this rotates the sprite to face its movement direction
+                    transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
                 }
                 else
                 {
                     moveDirection = Random.insideUnitCircle.normalized;
+                    rot_z = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;//this rotates the sprite to face its movement direction
+                    transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
                 }
             }
-            //if(no player)
             
             timer = MAXTIMER;
         }
@@ -78,11 +84,13 @@ public class ZombieControl : MonoBehaviour
 
     void UpdatePosition()
     {
-
+        //this is movement if it has a direction it moves in it
+        //otherwise it stays still
         if (moveDirection != Vector3.zero)
         {
             Vector3 newLocation = transform.position + (moveDirection * speed);
             _rigidbody.MovePosition(newLocation);
+            
         }
         else
         {
@@ -92,18 +100,17 @@ public class ZombieControl : MonoBehaviour
 
     void KillZombie()
     {
-        Destroy(this.gameObject);
+        Destroy(this.gameObject);//kills the game object
     }
 
     public void TakeDamage(float damage)
     {
-        Health -= damage;
+        Health -= damage;//taking damage
 
     }
     public void ReceiveKnockback(Vector3 knockback)
     {
-        float diff =  knockback.magnitude - resistance;
-        Debug.Log(diff);
+        float diff =  knockback.magnitude - resistance;//calculates knockback and sets the movement direction to match
         if (diff > 0f)
         {
             moveDirection = knockback.normalized * diff;
@@ -116,10 +123,11 @@ public class ZombieControl : MonoBehaviour
     {
         if (col.gameObject.tag == Constants.Strings.PLAYER_TAG)
         {
-            col.gameObject.SendMessage("TakeDamage", damage);
+            col.gameObject.SendMessage("TakeDamage", damage);//if it hits a player send a message for them to take damage
         }else if(col.gameObject.tag == Constants.Strings.ENVIRONMENT_TAG)
         {
-            Debug.Log("Bumped into something");
+            Debug.Log("Bumped into something");//when it bumps into the environment change direction
+            timer = 0;
         }
     }
 }
